5ef8e46729db38730aa4505495e4cdf82a3fefca
박나경 <akdldkdlel13@gmail.com>
2026-05-11 15:29:37 +0900

    feat: 초기 세팅

---
+++ app/src/main/java/com/example/plog/network/ApiClient.java
@@ -0,0 +1,35 @@
+package com.example.plog.network;
+
+import com.example.plog.util.Constants;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient() {
+        if (retrofit == null) {
+            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(logging)
+                    .build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(Constants.BASE_URL)
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+
+    public static ApiService getApiService() {
+        return getClient().create(ApiService.class);
+    }
+}
