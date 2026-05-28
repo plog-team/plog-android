383357c08cfffaa9dc6a6fba1d450af5fb41bf7b
idilla <113289555+idilla@users.noreply.github.com>
2026-05-28 18:47:58 +0900

    feat : 공식 API url 주소 입력
    API Key는 빈값 처리
    API 공식주소는 보안상 문제없음

--- app/src/main/java/com/example/plog/util/Constants.java
+++ app/src/main/java/com/example/plog/util/Constants.java
@@ -1,7 +1,7 @@
 package com.example.plog.util;
 
 public class Constants {
-    public static final String BASE_URL = "http://10.0.2.2:8080/";
+    public static final String BASE_URL = "";
 
     // Kakao REST API KEY
     public static final String KAKAO_REST_API_KEY = "";
@@ -11,10 +11,10 @@ public class Constants {
 
     // Kakao 주소 변환 API URL
     public static final String KAKAO_COORD_TO_ADDRESS_URL =
-            "";
+            "https://dapi.kakao.com/v2/local/geo/coord2address.json";
 
     // OpenWeather 현재 날씨 API URL
     public static final String OPENWEATHER_CURRENT_WEATHER_URL =
-            "";
+            "https://api.openweathermap.org/data/2.5/weather";
 
 }
